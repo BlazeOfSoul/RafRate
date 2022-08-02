aee17863be487283c33762ca840042e4fe7da6e8
Daniil <danielstudentbsu@gmail.com>
2022-08-02 09:36:29 +0300

    add files for minimal structure

---
+++ RafRate.API/Controllers/AuthenticateController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RafRate.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticateController : ControllerBase
+    {
+    }
+}
