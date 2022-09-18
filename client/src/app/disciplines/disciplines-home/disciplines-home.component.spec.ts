319932be4b9c2e2c0da76f69afd1edd0bc8758ec
Kolb Rostislav <rostislavkolb@gmail.com>
2022-09-18 22:23:19 +0300

    add: disciplines module
    fix: sort components for modules

---
+++ client/src/app/disciplines/disciplines-home/disciplines-home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DisciplinesHomeComponent } from './disciplines-home.component';
+
+describe('DisciplinesHomeComponent', () => {
+  let component: DisciplinesHomeComponent;
+  let fixture: ComponentFixture<DisciplinesHomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DisciplinesHomeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DisciplinesHomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
